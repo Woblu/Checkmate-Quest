74b485c855153f46b7445cccc267889752600496
Woblu <boaz.vega@proton.me>
2026-02-21 20:43:09 -0600

    Fix middleware invocation and add environment variables

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Define the routes that require the user to be logged in
 const isProtectedRoute = createRouteMatcher([
   '/play(.*)',
   '/campaign(.*)',
@@ -10,17 +9,13 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect();
+    (await auth()).protect();
   }
 });
 
-// This config tells Next.js EXACTLY when to run this middleware
 export const config = {
-  runtime: 'nodejs',
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
