7501aed539fb1dfab028ea2cf9108382a4179fd7
Woblu <boaz.vega@proton.me>
2026-02-21 20:12:00 -0600

    Hardcode official Clerk Edge middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+// Define the routes that require the user to be logged in
 const isProtectedRoute = createRouteMatcher([
   '/play(.*)',
   '/campaign(.*)',
   '/shop(.*)',
-  '/profile(.*)',
-])
+  '/profile(.*)'
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
-    auth.protect()
+    auth().protect();
   }
-})
+});
 
+// This config tells Next.js EXACTLY when to run this middleware
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/'],
-}
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
